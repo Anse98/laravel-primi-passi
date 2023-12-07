2ac2f8f22a153359528b073afbc6f9ffbf811163
Anse98 <lorenzo.anselmi98.com>
2023-12-07 14:00:28 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi passi</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="title">
+            <h1>{{ $title }}</h1>
+            <p>Ciao! Mi chiamo {{ $my_name }}</p>
+        </div>
+    </div>
+</body>
+
+</html>
